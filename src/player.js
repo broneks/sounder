8f3b128e9952e54a3f8055149127e5f6bb7c167b
Bronek Szulc <bronek@fastmail.fm>
2024-12-01 11:58:59 -0500

    play random notes

--- src/player.js
+++ src/player.js
@@ -1,9 +1,25 @@
-export function setupPlayer(element) {
-  const onClick = () => {
-    alert('click!')
+import { heartbeat } from 'webdaw-modules';
 
-    // where the magic happens
-  }
+export async function setupPlayer(element) {
+  await heartbeat.ready();
 
-  element.addEventListener('click', onClick)
+  const events = heartbeat.util.getRandomNotes({
+      minNoteNumber: 60,
+      maxNoteNumber: 100,
+      minVelocity: 30,
+      maxVelocity: 80,
+      numNotes: 60
+  });
+
+  const part = heartbeat.createPart();
+  part.addEvents(events);
+
+  const song = heartbeat.createSong({
+    parts: part,
+    useMetronome: true
+  });
+
+  element.addEventListener('click', () => {
+    song.play();
+  });
 }
