dbdc266d691aa639cd6e70b840f1beade97ef02b
Bronek Szulc <bronek@fastmail.fm>
2024-12-01 12:32:01 -0500

    play sequence

---
+++ README.md
@@ -0,0 +1,15 @@
+WEATHER MVP
+---
+
+- [x] get array of temp data
+- [x]  find min and max
+- [ ] normalize data to be between 1 and 22
+- [ ] map normalized data to "good notes" notes
+- [ ] create midi sequence (with velocity and duration)
+- [ ] play on button click
+
+FUTURE IDEAS
+---
+
+- "modifiers"
+  - different instruments for different datatype
--- src/player.js
+++ src/player.js
@@ -1,25 +1,45 @@
-import { heartbeat } from 'webdaw-modules';
+function playNoteInBrowser(note, duration) {
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-export async function setupPlayer(element) {
-  await heartbeat.ready();
+  // Convert MIDI note to frequency
+  const frequency = 440 * Math.pow(2, (note - 69) / 12);
 
-  const events = heartbeat.util.getRandomNotes({
-      minNoteNumber: 60,
-      maxNoteNumber: 100,
-      minVelocity: 30,
-      maxVelocity: 80,
-      numNotes: 60
+  const oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine'; // Waveform type: sine, square, sawtooth, triangle
+  oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+
+  const gainNode = audioCtx.createGain();
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+
+  oscillator.start();
+  oscillator.stop(audioCtx.currentTime + duration / 1000); // Stop after duration
+}
+
+// Function to play a sequence of notes
+function playSequence(sequence) {
+  let currentTime = 0;
+
+  sequence.forEach(({ note, duration }) => {
+    setTimeout(() => playNoteInBrowser(note, duration), currentTime);
+    currentTime += duration;
   });
+}
 
-  const part = heartbeat.createPart();
-  part.addEvents(events);
+export function setupPlayer(element) {
+  // A sequence of MIDI notes (C4, D4, E4, F4)
+  const sequence = [
+    { note: 60, duration: 500 }, // C4, 500ms
+    { note: 62, duration: 500 }, // D4, 500ms
+    { note: 64, duration: 500 }, // E4, 500ms
+    { note: 65, duration: 500 }, // F4, 500ms
+    { note: 60, duration: 500 }, // C4, 500ms
+    { note: 62, duration: 500 }, // D4, 500ms
+    { note: 64, duration: 500 }, // E4, 500ms
+    { note: 65, duration: 500 }, // F4, 500ms
+  ];
 
-  const song = heartbeat.createSong({
-    parts: part,
-    useMetronome: true
-  });
-
-  element.addEventListener('click', () => {
-    song.play();
+  element.addEventListener("click", () => {
+    playSequence(sequence);
   });
 }
